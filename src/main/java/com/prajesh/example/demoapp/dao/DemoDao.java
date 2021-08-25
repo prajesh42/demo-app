9452be6eddf5764088694e639528d853a6b0dd88
prajesh42 <panjiyarprajesh@yahoo.com>
2021-08-25 23:26:37 +0530

    build issue fixed

--- src/main/java/com/prajesh/example/demoapp/dao/DemoDao.java
+++ src/main/java/com/prajesh/example/demoapp/dao/DemoDao.java
@@ -3,9 +3,11 @@ package com.prajesh.example.demoapp.dao;
 import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import com.prajesh.example.demoapp.model.ProductInfo;
 
+@Repository
 public interface DemoDao extends CrudRepository<ProductInfo,Integer> {
 
 	public List<ProductInfo> findByCategoryId(String categoryId);
--- src/main/java/com/prajesh/example/demoapp/dao/DemoDaoImpl.java
+++ src/main/java/com/prajesh/example/demoapp/dao/DemoDaoImpl.java
@@ -3,11 +3,11 @@ package com.prajesh.example.demoapp.dao;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Component;
 
 import com.prajesh.example.demoapp.model.ProductInfo;
 
-@Repository
+@Component
 public class DemoDaoImpl {
 
 	@Autowired
